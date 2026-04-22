8d386f8b164e80efb7048276e180a5a18b80796f
kn0vd3h1 <kn0vd3h1@rolf.ovh>
2026-04-22 22:55:38 +0000

    Add temporary change

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+// This is a dummy change to trigger CI
 export { AnalyticsUrlParams } from './src/url-params';
 export { Client, Config, createClient, createNoopClient } from './src/client';
 export { createMarketingClient } from './src/marketingClient';
